39f32e5db4769775694c14ce9e2bc7f35a1d4451
Andrey <alad1chek@gmail.com>
2019-02-23 20:05:52 +0200

    fix: add fixture functions

--- app/repository/request_repository.py
+++ app/repository/request_repository.py
@@ -3,13 +3,15 @@ import os
 from motor.motor_tornado import MotorClient
 from tornado.log import app_log
 
+
 class RequestRepository:
     def __init__(self):
         db_url = os.getenv("MONGODB_URL")
         db_name = os.getenv("MONGODB_DB")
         self.collection = MotorClient(db_url)[db_name].request
 
-    async def get_requests_by_user(self, user_id):
+    async def get_requests_by_user(self, user_id, limit=None):
+        # filter by update date
         return {{'name': 'new','userId': user_id,'status': 0}, {'name': 'inreview','userId': user_id,'status': 2},{'name': 'closed','userId': user_id,'status': 1}}
 
     async def get_reviewer_liked_commented_requests(self, user_id):
@@ -17,3 +19,9 @@ class RequestRepository:
 
     async def get_all_reviewer_commented_requests(self, user_id):
         return {{'name': 'new','userId': user_id,'status': 0}, {'name': 'inreview','userId': user_id,'status': 2},{'name': 'closed','userId': user_id,'status': 1}}
+
+    async def create_request(self): 
+        return True
+
+    async def comment_request(self, request_id, comment): 
+        return True

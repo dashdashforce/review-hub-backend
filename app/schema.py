d9b6d852caa65cf1bc06d8f4a5a3f8f86b45f827
Maks <maks@MacBook-Pro-van-Bintime.local>
2019-02-24 00:46:14 +0200

    feat: add langs

--- app/schema.py
+++ app/schema.py
@@ -6,29 +6,44 @@ import graphene
 from tornado import gen
 from tornado.log import app_log
 
-class PullRequest(graphene.ObjectType):
-    id = graphene.String()
-    name = graphene.String()
-    userId = graphene.String()
-    status = graphene.String()
-    langs = graphene.String()
-    reviewers = graphene.List()
-    comments = graphene.List()
-
 class Language(graphene.ObjectType):
     id = graphene.String()
     color = graphene.String()
     name = graphene.String()
+
+    @classmethod
+    def map(cls, lang_dict):
+        return Language(
+            lang_dict['id'],
+            lang_dict['color'],
+            lang_dict['name']
+            )
+
+class Comment(graphene.ObjectType):
+    text = graphene.String()
+    reviewerId = graphene.String()
+    status = graphene.String()
+
+class PullRequest(graphene.ObjectType):
+    id = graphene.String()
+    name = graphene.String()
+    code = graphene.String()
+    userId = graphene.String()
+    status = graphene.String()
+    langs = graphene.List(Language)
+    comments = graphene.List(Comment)
 
 class User(graphene.ObjectType):
     id = graphene.String()
     token = graphene.String()
     image_url = graphene.String()
     langs = graphene.List(Language)
-    pull_requests = 
+    pull_requests = graphene.List(PullRequest)
 
 class Query(graphene.ObjectType):
     user = graphene.Field(User, id=graphene.String())
+
+    pull_request = graphene.Field(PullRequest, id=graphene.String())
 
     def resolve_test(self, info):
         return 'Hello world {}'.format(info.context.authentication)

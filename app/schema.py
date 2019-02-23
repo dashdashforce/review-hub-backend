2acf803059d84e71cbbaf9686dced061423335a2
Maks <maks@MacBook-Pro-van-Bintime.local>
2019-02-23 23:34:44 +0200

    feat: add user schemeb

--- app/schema.py
+++ app/schema.py
@@ -6,12 +6,33 @@ import graphene
 from tornado import gen
 from tornado.log import app_log
 
+class PullRequest(graphene.ObjectType):
+    id = graphene.String()
+    name = graphene.String()
+    userId = graphene.String()
+    status = graphene.String()
+    langs = graphene.String()
+    reviewers = graphene.List()
+    comments = graphene.List()
+
+class Language(graphene.ObjectType):
+    id = graphene.String()
+    color = graphene.String()
+    name = graphene.String()
+
+class User(graphene.ObjectType):
+    id = graphene.String()
+    token = graphene.String()
+    image_url = graphene.String()
+    langs = graphene.List(Language)
+    pull_requests = 
 
 class Query(graphene.ObjectType):
-    test = graphene.String()
+    user = graphene.Field(User, id=graphene.String())
 
     def resolve_test(self, info):
         return 'Hello world {}'.format(info.context.authentication)
+
 
 
 schema = graphene.Schema(query=Query)

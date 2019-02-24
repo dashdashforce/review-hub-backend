fe45f514e7c0d980f79faf76f72e7d34a6f95664
Oleg Lipskiy <lolego1601@gmail.com>
2019-02-24 05:06:16 +0200

    feat: add repo log

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,5 +1,6 @@
 from ..repository import UserRepository
 from tornado.httpclient import HTTPError
+from tornado.log import app_log
 
 
 class UserService:
@@ -8,6 +9,7 @@ class UserService:
         self.user_repository = user_repository
 
     async def get_user(self, user_id):
+        app_log.debug('User service: get user with id: {}'.format(user_id))
         user = await self.user_repository.get_user(user_id)
         if not user:
             raise HTTPError(404, 'User not found')

b407168de275020cff4affd1041a6e7ae424a633
Oleg Lipskiy <lolego1601@gmail.com>
2019-02-24 03:36:40 +0200

    feat: add oauth integration

--- app/clients/github_client.py
+++ app/clients/github_client.py
@@ -24,19 +24,26 @@ class GithubClient:
                     e, e.response.body)
             )
             raise e
+        app_log.debug(response.body)
         return json_decode(response.body)
 
     def _build_authorization_request(self, code):
         url = 'https://github.com/login/oauth/access_token'
-        body = (
-            ('client_id', settings.GITHUB_CLIENT_ID),
-            ('client_secret', settings.GUTHUB_CLIENT_SECRET),
-            ('code', code)
-        )
+        body = {
+            'client_id': settings.GITHUB_CLIENT_ID,
+            'client_secret': settings.GUTHUB_CLIENT_SECRET,
+            'code': code
+        }
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'User-Agent': settings.USER_AGENT
+        }
         return HTTPRequest(
             url=url,
             method='POST',
-            body=urlencode(body)
+            body=json_encode(body),
+            headers=headers
         )
 
     async def fetch_user(self, access_token):
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -12,12 +12,11 @@ class AuthService:
         self.user_transformer = user_transformer
 
     async def get_token(self, code):
-        # auth_response = await self.github_client.authorize(code)
-        # access_token = auth_response['access_token']
-        access_token = 'e10c0ab23e9e5da5d54f3f17e58994bcaf98d227'
+        auth_response = await self.github_client.authorize(code)
+        access_token = auth_response['access_token']
         user = await self.github_client.fetch_user(access_token)
         app_log.debug('Github user: {}'.format(user))
-        self.create_user( user['viewer'])
+        self.create_user(user['viewer'])
         return encode({
             'id': user['viewer']['id'],
             'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=settings.AUTH_EXPIRE)},

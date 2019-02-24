24b389e8b08bf831f360b516d37aefcfc57bb433
Oleg Lipskiy <lolego1601@gmail.com>
2019-02-24 05:12:00 +0200

    fix: minor fixes

--- app/entity_transformer/request_transformer.py
+++ app/entity_transformer/request_transformer.py
@@ -1,12 +1,13 @@
 from .base_tentity_transformer import BaseEntityTransformer
 
+
 class RequestTransformer(BaseEntityTransformer):
     def create_entity(self, gitdata):
         return {
             '_id': gitdata['id'],
             'user_id': gitdata['user_id'],
             'name': 'temp',
-            'repo_name': gitdata['repo_name']
+            'repo_name': gitdata['repo_name'],
             'token': 'temp',
             'status': 0,
             'langs': [],
